e1876b5a42cfa3dbc0ccb4ca7c1c03fae4caa117
Paul Mayr <mayr.paul@gmx.net>
2025-05-28 15:33:53 +0200

    added ExplodARController

---
+++ explodAR/Assets/Scripts/ExplodARController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ExplodARController : MonoBehaviour
+{
+    [Tooltip("A template to use as a button for exploding objects")]
+    public GameObject explodeButtonTemplate;
+
+    [Tooltip("How far the button should be placed from its object")]
+    public float buttonDistanceFromObject;
+
+    [Tooltip("How many seconds explosions take")]
+    public float explosionDuration;
+
+    public static ExplodARController instance;
+    private void Awake()
+    {
+        instance = this;   
+    }
+}
